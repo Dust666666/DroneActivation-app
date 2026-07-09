b031224cf5d3fdb63a82d157b1167bd0a2f1206a
Dust666666 <2644270443@qq.com>
2026-07-09 14:36:12 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+
+    id("com.android.application")
+
+    id("org.jetbrains.kotlin.android")
+
+    //id("com.chaquo.python")
+
+    id("org.jetbrains.kotlin.plugin.compose")
+
+}
+
+android {
+
+    namespace = "com.drone.activation"
+
+    compileSdk = 35
+
+    defaultConfig {
+
+        applicationId = "com.drone.activation"
+
+        minSdk = 29
+
+        targetSdk = 35
+
+        versionCode = 1
+
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+
+    buildTypes {
+
+        release {
+
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+        }
+
+    }
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_17
+
+        targetCompatibility = JavaVersion.VERSION_17
+
+    }
+
+    kotlinOptions {
+
+        jvmTarget = "17"
+
+    }
+
+    buildFeatures {
+        compose = true
+        // 明确关闭 buildConfig，解决废弃警告
+        buildConfig = false
+    }
+
+}
+
+dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2025.01.01"))
+
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+}
---
+++ app/proguard-rules.pro
@@ -0,0 +1,9 @@
+# DroneActivation
+
+# 当前版本先不添加混淆规则
+
+# 后续加入：
+# Chaquopy
+# MAVLink
+# Retrofit
+# Gson
